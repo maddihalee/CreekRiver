90d831a122e64efb4994e3fcf494fd6a91b2a86a
Maddi Cook <maddihcook@gmail.com>
2023-08-15 18:28:21 -0500

    get campsites

--- CreekRiver/Program.cs
+++ CreekRiver/Program.cs
@@ -26,6 +26,16 @@ builder.Services.Configure<JsonOptions>(options =>
 
 var app = builder.Build();
 
+app.MapGet("/api/campsites", (CreekRiverDbContext db) =>
+{
+    return db.Campsites.ToList();
+});
+
+app.MapGet("/api/campsites/{id}", (CreekRiverDbContext db, int id) =>
+{
+    return db.Campsites.Include(c => c.CampsiteType).Single(c => c.Id == id);
+});
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
